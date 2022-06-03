ad53d868ba03baddef597f04ab8f4c08618da6ba
Ongezwa <ongezwaf27@gmail.com>
2022-06-03 14:46:35 +0200

    first commit

---
+++ main.js
@@ -0,0 +1,10 @@
+import './style.css'
+
+document.querySelector('#app').innerHTML = "I 💚 Alpine JS!";
+  
+
+
+
+// <h1>Hello Vite!</h1>;
+//   <a href="https://vitejs.dev/guide/features.html" target="_blank">Documentation</a
+
